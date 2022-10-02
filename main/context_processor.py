c9dfc002e3d5e480c30ec8f8b5dd85a6a8f0f3b1
mcmc101001 <96778937+mcmc101001@users.noreply.github.com>
2022-10-02 12:47:00 +0800

    fixed context_processor

--- main/context_processor.py
+++ main/context_processor.py
@@ -9,10 +9,12 @@ from .models import CustomUser, Request, Task, Reward, User, Follow, Cart
 
 from datetime import datetime
 
-@login_required(login_url='/main/login')
 def cart_count(request):
-    current_user = CustomUser.objects.get(user=request.user)
-    cart_count = Cart.objects.filter(user=current_user).count()
-    return {
-        'cart_count' : cart_count
-    }
+    try:
+        current_user = CustomUser.objects.get(user=request.user)
+        cart_count = Cart.objects.filter(user=current_user).count()
+        return {
+            'cart_count' : cart_count
+        }  
+    except TypeError:
+        return []
